7d62d0c615373eee7acf774edbebfe090aec7269
guy.shababo <guy4497@gmail.com>
2022-11-04 21:45:01 +0200

    home 3

---
+++ 10pow.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+double ten_pow(int n)
+{
+	int times;
+	double result = 1, base;
+	if (n < 0)
+	{
+		base = 0.1;
+		times = -n;
+	}
+	else
+	{
+		base = 10;
+		times = n;
+	}	
+	for(int i = 0; i < times; i++)
+	{
+		result *= base;
+	}
+	printf("%f", result);
+	return base;
+}
+
+int main()
+{
+	ten_pow(-3);
+}
