1140619d7b72992b0a9833eb35be202e485896f3
Guy.Shababo <guy4497@gmail.com>
2022-11-07 15:47:26 +0200

    valswap + copyarray working

--- c/pointers1/copy_array.c
+++ c/pointers1/copy_array.c
@@ -1,21 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-void CopyArray(int *array)
+void CopyArray(int *array, int *new_array, int array_size)
 {
-	int array_size = sizeof(array) / sizeof(array[0]);
-	int* new_array = malloc(array_size * sizeof(int));
-	for(int i=0; i<array_size;i++)
-	{
+	int i = 0;
+	for(; i < array_size; i++)
 		new_array[i] = array[i];
-		printf("old: %d, copy: %d\n", array[i], new_array[i]);
-	}
 }
 
-
-int main()
-{
-	int array[] = {0, 1, 2, 3, 4};
-	CopyArray(array);
-	return 0;	
-}
