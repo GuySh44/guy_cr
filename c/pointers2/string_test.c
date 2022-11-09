510cf28706446493d5cc639375e560c1642315e2
Guy.Shababo <guy4497@gmail.com>
2022-11-09 10:52:31 +0200

    update

--- c/pointers2/string_test.c
+++ c/pointers2/string_test.c
@@ -30,38 +30,51 @@ int main()
 	printf("\n");
 	
 	printf("check short destination n < src\n");
-	printf("old value src:%s, short_dest:", src);
+	printf("old value src:");
+	for(; i < 7; i++)
+		printf("'%02x'",*(src+i));
+	printf(", short_dest:");
 	for(; i < 6; i++)
-		printf("%c",*(short_dest+i));
+		printf("'%02x'",*(short_dest+i));
+	
+	
+	
 	printf("\n");
 	StrNcpy(short_dest,src,6);
-	printf("new value src:%s, short_dest:", src);
+	
+	
+	printf("new value src:");
+	for(; i < 7; i++)
+		printf("'%02x'",*(src+i));
+	printf(", short_dest:");
 	for(; i < 6; i++)
-		printf("%c",*(short_dest+i));
+		printf("'%02x'",*(short_dest+i));
+		
+		
 	printf("\n");
 	printf("\n");
 	
 	printf("check long destination n > src \n");
 	printf("old value src:%s, long_dest:", src);
 	for(; i < 9; i++)
-		printf("%c",*(long_dest+i));
+		printf("%02x",*(long_dest+i));
 	printf("\n");
 
 	StrNcpy(long_dest,src,9);
 	printf("new value src:%s, long_dest:", src);
 	for(; i < 9; i++)
-		printf("%c",*(long_dest+i));
+		printf("%02x",*(long_dest+i));
 	printf("\n");
 	
 	printf("original string n compare with high n:%d\n",strncmp("abc","abd",5));
 	printf("\n");
 	printf("check case compare:\n");
 	printf("mine:%d \n",StrCasecmp("Arz","arz"));
-	printf("original:%d \n",strcasecmp("Arz","arz"));
+
 	printf("mine:%d \n",StrCasecmp("arz","Arz"));
-	printf("original:%d \n",strcasecmp("arz","Arz"));
+
 	printf("mine:%d \n",StrCasecmp("arz","Arzsd"));
-	printf("original:%d \n",strcasecmp("bA","Ab"));
+
 	
 	
 	
