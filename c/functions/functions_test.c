fd1b2d68dba9a6a65b825cae96ab78dc580e2e94
Guy.Shababo <guy4497@gmail.com>
2022-11-16 09:01:47 +0200

    added testing interface

--- c/functions/functions_test.c
+++ c/functions/functions_test.c
@@ -1,8 +1,21 @@
 #include"functions.h"
 int main()
 {
-	lutImplement();
-	switchImplement();
-	ifImplement();
+	int i;
+	printf("choose implementation:\n[1] LUT\n[2] switch\n[3] if/else\n");
+	scanf("%d",&i);
+	printf("Excellent choice\n");
+	switch(i)
+	{
+		case 1:
+			lutImplement();
+			break;
+		case 2:
+			switchImplement();
+			break;
+		case 3:
+			ifImplement();
+			break;
+	}
 	return 0;
 }
