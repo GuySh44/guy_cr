d96e9d2792ce814dfceaf1ac10e5d87831c6bff5
Guy.Shababo <guy4497@gmail.com>
2022-11-24 18:03:05 +0200

    structs exer complete

--- c/structs/structs.h
+++ c/structs/structs.h
@@ -1,12 +1,23 @@
 #ifndef __STRUCTS_H__
 #define __STRUCTS_H__
-#include<string.h> 
-#include<assert.h>
-#include<stdio.h>
-#include<stdlib.h>
+#include<string.h> /* strlen, strcat */
+#include<assert.h> /* assert */
+#include<stdio.h> /* printf */
+#include<stdlib.h> /* malloc calloc */
 
+/* reviewer: or */
 #define DIGIT_ADD 48
 
+#define MAX2(a, b) (((a) > (b)) ? (a) : (b))
+
+#define MAX3(a, b, c) (((MAX2(a,b) > MAX2(b,c)) ? MAX2(a,b) : MAX2(b,c)))
+
+#define SIZEOF_VAR(type) (char *)(&type+1)-(char*)(&type)
+
+#define SIZEOF_TYPE(type) ((char *)(((type *)0) + 1) - (char *)(((type *)0)))
+
+
+/* struct holding a certain value of unknown type, and function pointers to address different implementations of functionality */
 typedef struct elements_array_t
 {
 void *data;
@@ -15,18 +26,22 @@ void (*clean)(void*);
 void (*print)(void*);
 } ElementsArray;
 
+/* "cleaning" functions to free each type */
+
 void cleanInt(void* num);
 
 void cleanFloat(void* num);
 
 void cleanString(void* str);
 
+/* adding functions to add int values in the required way */
 void addInt(void *data ,int num);
 
 void addFloat(void *data, int num);
 
 void addString(void *string, int num);
 
+/* printing functions to print each type */
 void printInt(void *data);
 
 void printFloat(void *data);
--- c/structs/structs_test.c
+++ c/structs/structs_test.c
@@ -1,11 +1,18 @@
 #include"structs.h"
+
 int main()
 {
+	int a = 51, b = 48, c = 52;
+	char check_type = 'c';
+	
+	
+	
 	extern char* strdup(const char*);
 	char *str2 = NULL;
 	char *str1 = "chapter";
 	int i = 0, j = 0, k = 0, l = 0;
 	ElementsArray arr[5];
+	ElementsArray size;
 	
 	arr[0].data = (float*)malloc(sizeof(arr[0].data));
 	*((float *)(arr[0].data)) = 4.2;
@@ -55,5 +62,20 @@ int main()
 	{
 		arr[l].clean(arr[l].data);
 	}
+	
+	
+	
+	
+	
+	
+	
+	printf("max of a = 51 and b = 48\t%d\n", MAX2(a, b));
+	printf("max of a = 51 and b = 48 and c = 52\t%d\n", MAX3(a, b, c));
+	printf("SIZEOF_VAR int\t%ld\n",SIZEOF_VAR(a));
+	printf("SIZEOF_VAR char\t%ld\n",SIZEOF_VAR(check_type));
+	printf("SIZEOF_VAR ElementsArray\t%ld\n",SIZEOF_VAR(size));
+	printf("SIZEOF_TYPE int\t%ld\n",SIZEOF_TYPE(int));
+	printf("SIZEOF_TYPE char\t%ld\n",SIZEOF_TYPE(char));
+	printf("SIZEOF_TYPE ElementsArray\t%ld\n",SIZEOF_TYPE(ElementsArray));
 	return 0;
 }
