03a955025874d0e9dfa01c0292aecd54cbbbfd91
Guy.Shababo <guy4497@gmail.com>
2023-01-01 18:59:14 +0200

    working recursive no memory problems + comments

--- ds/src/recursion.c
+++ ds/src/recursion.c
@@ -20,12 +20,9 @@ int FibonacciRegular(int element_index)
 	return curr;
 }
 
+/* 0,1 are "edge" cases and we need to check for them */
 int FibonacciRecursive(int element_index)
 {
-	if(element_index == 2)
-	{
-		return 1;
-	}
 	if(element_index == 1)
 	{
 		return 1;
@@ -60,6 +57,7 @@ size_t StrlenRecursive(const char *s)
 	return 1 + StrlenRecursive(s+1);
 }
 
+
 int StrcmpRecursive(const char *s1, const char *s2)
 {
 	if(*s1 != *s2 || ('\0' == *s1 || '\0' == *s2))
@@ -69,6 +67,7 @@ int StrcmpRecursive(const char *s1, const char *s2)
 	return StrcmpRecursive(s1+1,s2+1);
 }
 
+/*we find src end and copy from end to start*/
 char *StrcpyRecursive(char *dest, const char *src)
 {
 	if('\0' == *src)
@@ -91,6 +90,7 @@ char *StrcatRecursive(char *dest, const char *src)
 	return dest;
 }
 
+/*we find first occurance of needle by iterating over haystack, if we get to end of haystack we return NULL*/
 char *StrstrRecursive(const char *haystack, const char *needle)
 {
 	if(0 == strncmp(haystack, needle, StrlenRecursive(needle)))
@@ -104,19 +104,28 @@ char *StrstrRecursive(const char *haystack, const char *needle)
 	return StrstrRecursive(haystack + 1, needle);
 }
 
+/*we use Insert sort algorithm implemented for a stack. our starting point is a stack with 1 element inside it, and thats why we can deduct the stack is sorted in each step, then we insert the element in the right place just like insert sort*/
 static void InsertSortStack(stack_t *stack, int elem)
 {
-	int stack_top = *((int*)StackPeek(stack));
-	if(stack_top < elem || IsStackEmpty(stack))
+	int stack_top;
+	if(IsStackEmpty(stack))
 	{
 		StackPush(stack, &elem);
 		return;
 	}
+	stack_top = *((int*)StackPeek(stack));
 	StackPop(stack);
+	if(stack_top < elem)
+	{
+		StackPush(stack, &stack_top);
+		StackPush(stack, &elem);
+		return;
+	}
 	InsertSortStack(stack, elem);
 	StackPush(stack, &stack_top);
 } 
 
+/*Base recursive function for the Sort, its main use is to empty the stack, and keep the last element we popped, then calling the insert function to insert the popped element in the right place*/
 void SortStack(stack_t *stack)
 {
 	int stack_top;
