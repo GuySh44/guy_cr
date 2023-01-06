f0e42f14326263592dcd0adf5c9eee97aed18c9e
Guy.Shababo <guy4497@gmail.com>
2023-01-06 13:32:15 +0200

    circular array

--- quizzes/find_circular.c
+++ quizzes/find_circular.c
@@ -12,30 +12,30 @@ size_t FindCircular(int *arr, size_t size, int target)
 	{
 		if (arr[top] > arr[mid])
 		{
-			top = mid;
+			top = mid - 1;
 		}
 		else
 		{
-			bottom = mid;
+			bottom = mid + 1;
 		}
 		mid = (top + bottom) / 2;
 	}
 	offset = mid;
-	top = (offset - 1) % size;
+	top = (offset + size - 1) % size;
 	bottom = offset;
-	mid = bottom + (((size - ((long)(bottom - offset) % size)) + (size - (((long)(top - offset) % size)))) / 2);
+	mid = (bottom + ((top + size - offset) % size - (bottom + size - offset) % size) / 2) % size;
 	
 	while(arr[mid] != target)
 	{
 		if(arr[mid] > target)
 		{
-			top = mid;
+			top = (mid + size - 1) % size;
 		}
 		else
 		{
-			bottom = mid;
+			bottom = (mid + 1) % size;
 		}
-		mid = bottom + (((size - ((long)(bottom - offset) % size)) + (size - (((long)(top - offset) % size)))) / 2);
+		mid = (bottom + ((top + size - offset) % size - (bottom + size - offset) % size) / 2) % size;
 	}
 	return mid;
 }
@@ -45,8 +45,10 @@ int main()
 	int arr[6] = {7, 8, 11, 1, 4, 5};
 	int arr2[7] = {7, 1, 2, 3, 4, 5, 6};
 	int arr3[7] = {2, 3, 4, 5, 6, 7, 1};
+	int arr4[7] = {1, 4, 8, 12, 15, 16};
 	printf("11 index in arr[6] = {7, 8, 11, 1, 4, 5} is:\t%lu\n",(FindCircular(arr, 6, 11)));
 	printf("5 index in arr2[7] = {7, 1, 2, 3, 4, 5, 6} is:\t%lu\n",(FindCircular(arr2, 7, 5)));
 	printf("3 index in arr3[7] = {2, 3, 4, 5, 6, 7, 1} is:\t%lu\n",(FindCircular(arr3, 7, 3)));
+	printf("12 index in arr4[7] = {1, 4, 8, 12, 15, 16} is:\t%lu\n",(FindCircular(arr4, 7, 12)));
 	return 0;
 }
