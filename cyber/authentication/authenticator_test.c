07da7969b22187c97502b441117a23cd1621cad9
Guy.Shababo <guy4497@gmail.com>
2023-01-19 18:13:20 +0200

    added some tests

--- cyber/authentication/authenticator_test.c
+++ cyber/authentication/authenticator_test.c
@@ -16,22 +16,26 @@ int NoGuyAllowed(const char *password)
 int main()
 {
 	AuthConfigAuthenticator("./test_dir/users_db", NoGuyAllowed);
-	printf("%d\n",AuthAddUser("guy","guy"));
-	printf("%d\n",AuthAddUser("guy","passcode"));
-	printf("%d\n",AuthAddUser("", ""));
-	printf("%d\n",AuthAddUser("", "123"));
-	printf("%d\n",AuthAddUser("123", ""));
-	printf("%d\n",AuthAddUser(" 345", ""));
-	printf("%d\n",AuthAddUser("3:45", ""));
-	printf("%d\n",AuthAddUser("345\n", ""));
-	printf("%d\n",AuthAddUser("3:4\n5 ", ""));
-	printf("%d\n",AuthAddUser("345", ""));
+	printf("should get 5 %d\n",AuthAddUser("guy","guy"));
+	printf("should get 0 %d\n",AuthAddUser("guy","passcode"));
+	printf("should get 0 %d\n",AuthAddUser("", ""));
+	printf("should get 1 %d\n",AuthAddUser("", "123"));
+	printf("should get 0 %d\n",AuthAddUser("123", ""));
+	printf("should get 3 %d\n",AuthAddUser(" 345", ""));
+	printf("should get 3 %d\n",AuthAddUser("3:45", ""));
+	printf("should get 3 %d\n",AuthAddUser("345\n", ""));
+	printf("should get 3 %d\n",AuthAddUser("3:4\n5 ", ""));
+	printf("should get 0 %d\n",AuthAddUser("345", ""));
+	printf("should get 4 %d\n",AuthAddUser("111111111111111111111111111111111111", ""));
+	printf("should get 4 %d\n",AuthAddUser("", "111111111111111111111111111111111111"));
 	
-	printf("%d\n",AuthAuthenticator("guy","passcode"));
-	printf("%d\n",AuthAuthenticator("guy","passcode2"));
+	printf("should get 0 %d\n",AuthAuthenticator("guy","passcode"));
+	printf("should get 3 %d\n",AuthAuthenticator("guy","passcode2"));
 	
-	printf("%d\n",AuthDeleteUser("guy"));
-	printf("%d\n",AuthDeleteUser("123"));
-	printf("%d\n",AuthAuthenticator("guy","passcode"));
+	printf("should get 0 %d\n",AuthDeleteUser("guy"));
+	printf("should get 0 %d\n",AuthDeleteUser("123"));
+	printf("should get 1 %d\n",AuthDeleteUser("123"));
+	
+	printf("should get 1 %d\n",AuthAuthenticator("guy","passcode"));
 	return 0;
 }
