c0439d8b9fddbb5bb65ce22c304496ddfd238156
Guy.Shababo <guy4497@gmail.com>
2023-02-22 08:51:53 +0200

    division fix

--- python/exceptions/division.py
+++ python/exceptions/division.py
@@ -1,12 +1,13 @@
 def div(a, b):
     try:
         return a/b 
-    except TypeError:
-        print("ayo thats not a division type")
-        raise TypeError("shaggy this aint a division type")
     except ZeroDivisionError:
         print("tried to divide by zero")
         return None
+    except TypeError as e:
+        print("ayo thats not a division type")
+        raise TypeError("shaggy this aint a division type") from e
+    
     
 print(div(1,0))
 print(div(5,2))
