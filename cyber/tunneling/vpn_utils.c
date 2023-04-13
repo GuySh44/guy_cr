bee1412fc924db0f6fdfd3f271bd523ed18b2a4b
guy <guy4497@gmail.com>
2023-04-13 13:31:50 +0300

    from client

--- cyber/tunneling/vpn_utils.c
+++ cyber/tunneling/vpn_utils.c
@@ -14,20 +14,3 @@ int run(char *cmd)
 	return 0;
 }
 
-sudo apt-get update
-sudo apt-get install net-tools
-sudo apt-get install wireshark
-sudo apt-get install openssh-server 
-sudo apt-get install curl
-sudo apt install git
-sudo apt install apache2
-sudo apt-get install -y ca-certificates
-sudo apt install libnss3-tools
-sudo apt install python3-pip
-sudo apt-get install python3-scapy
-sudo apt install python-dev-is-python3 
-sudo apt-get install chromium
-sudo apt install libnss3-tools
-sudo apt install inetutils-traceroute 
-sudo apt install cgdb
-
